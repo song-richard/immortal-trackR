db91ab933aa1f7147e65add13ac97cf450a4c570
Richard Song <rsong1993@gmail.com>
2024-03-23 16:51:43 -0700

    Added MenuIcon to Appbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,49 @@
 import { AppBar, Container, Toolbar } from "@mui/material";
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Navbar() {
     return (
         <AppBar>
             <Container>
+                
                 <Toolbar>
-                <Typography
-                    variant="h6"
-                    noWrap
-                    component="a"
-                    href="#app-bar-with-responsive-menu"
-                    sx={{
-                        mr: 2,
-                        display: { xs: 'none', md: 'flex' },
-                        fontFamily: 'monospace',
-                        fontWeight: 700,
-                        letterSpacing: '.3rem',
-                        color: 'inherit',
-                        textDecoration: 'none',
-                    }}
-                >
-                    Immortal TrackR
-                </Typography>
+
+                    <Box>
+                        <IconButton
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        // onClick={handleOpenNavMenu}
+                        color="inherit"
+                        >
+                        <MenuIcon />
+                        </IconButton>
+                    </Box>
+
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Immortal TrackR
+                    </Typography>
+
                 </Toolbar>
+
             </Container>
         </AppBar>
     )
