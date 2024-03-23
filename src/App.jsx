ae2c7ca8d9c053b56ea33fa2d646e421e7e95e47
Richard Song <rsong1993@gmail.com>
2024-03-23 16:33:27 -0700

    Imported MUI modules. Added initial Appbar from MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from "./components/NavBar"
 
 function App() {
 
   return (
     <>
+      <Navbar></Navbar>
       <h1>Test</h1>
     </>
   )
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import { AppBar } from "@mui/material";
+
+export default function Navbar() {
+    return (
+        <AppBar>
+            <h2>APP BAR</h2>
+        </AppBar>
+    )
+}
