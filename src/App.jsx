2a429370adb89b706b59a6e183026d779bb82faa
Richard Song <rsong1993@gmail.com>
2024-03-23 16:46:14 -0700

    Added Container and Toolbar with Typography text of 'Immortal Trackr'

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
-      <h1>Test</h1>
     </>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,30 @@
-import { AppBar } from "@mui/material";
+import { AppBar, Container, Toolbar } from "@mui/material";
+import Typography from '@mui/material/Typography';
 
 export default function Navbar() {
     return (
         <AppBar>
-            <h2>APP BAR</h2>
+            <Container>
+                <Toolbar>
+                <Typography
+                    variant="h6"
+                    noWrap
+                    component="a"
+                    href="#app-bar-with-responsive-menu"
+                    sx={{
+                        mr: 2,
+                        display: { xs: 'none', md: 'flex' },
+                        fontFamily: 'monospace',
+                        fontWeight: 700,
+                        letterSpacing: '.3rem',
+                        color: 'inherit',
+                        textDecoration: 'none',
+                    }}
+                >
+                    Immortal TrackR
+                </Typography>
+                </Toolbar>
+            </Container>
         </AppBar>
     )
 }
